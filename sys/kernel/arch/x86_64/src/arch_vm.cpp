2cd5dcb5850e822262d64a654e25df7cc9a77218
Kelmar Firesun <kfiresun@gmail.com>
2026-08-01 02:35:51 -0500

    More work on getting x64 booting. - Fixed issue with boot.S not calling preinit() - boot.S now using EAX and EBX, multiboot only passes 32-bit values. - Correcting pop order of eax/ebx when we're getting back from the stack. - Fixed issue were EAX was getting cleared because of use of rdmsr and wrmsr instructions.

--- sys/kernel/arch/x86_64/src/arch_vm.cpp
+++ sys/kernel/arch/x86_64/src/arch_vm.cpp
@@ -21,10 +21,36 @@ uint32_t g_BootMagic; /* Value from EAX register */
 uint32_t g_Multiboot; /* Value from EBX register */
 
 /********************************************************************************************************************/
+/*
+ * These are defined in the linker script.
+ */
+
+/// @brief Physical memory location of the start of the kernel.
+extern "C" uintptr_t _kernel_phys_start;
+
+/// @brief Physical memory location of the end of the kernel.
+extern "C" uintptr_t _kernel_end;
+
+constexpr void *kernel_start = &_kernel_phys_start;
+
+constexpr void *kernel_end = &_kernel_end; //VIRT_2_PHYS(&_kernel_end);
+
+/********************************************************************************************************************/
 
 void arch::InitBootMemory(KernelArgs *ka)
 {
-    (void)ka;
+    Debug::PrintF("ENTER: Arch::InitBootMemory()\r\n");
+
+    Debug::PrintF("Boot Magic: 0x%08X\r\n", g_BootMagic);
+    Debug::PrintF("Multiboot : 0x%08X\r\n", g_Multiboot);
+    
+    // Figure out where we live in physical memory.
+    ka->KernelCode.Base = reinterpret_cast<uintptr_t>(&kernel_start);
+    uintptr_t kend = reinterpret_cast<uintptr_t>(&kernel_end);
+
+    ka->KernelCode.Length = kend - ka->KernelCode.Base;
+
+    Debug::PrintF("Kernel: %p 0x%08X\r\n", ka->KernelCode.Base, ka->KernelCode.Length);
 }
 
 /********************************************************************************************************************/
